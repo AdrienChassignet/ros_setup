bf6d4bcf97828f29354b85129abfb78dbfe3dd97
AdrienChassignet <47776406+AdrienChassignet@users.noreply.github.com>
2019-07-31 15:46:36 +0200

    Update joy_control_dynamixel.cpp
    addition of subscriber to update the amplitude of the movement
    also add lambda function for min and max

--- ros_packages/joy_control_dynamixel/src/joy_control_dynamixel.cpp
+++ ros_packages/joy_control_dynamixel/src/joy_control_dynamixel.cpp
@@ -5,128 +5,127 @@
 #include <unistd.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
+#include <std_msgs/Int16.h>
 #include <dynamixel_workbench_msgs/DynamixelCommand.h>
 #include <trajectory_msgs/JointTrajectory.h>
 //#include <trajectory_msgs/JointTrajectoryPoint.h>
 
-#define MAX_POS 2550
-#define ZERO_POS 2048
-#define MIN_POS 1550
-
-class Joy2Dynamixel
+namespace joy2dynamixel
 {
-public:
-  Joy2Dynamixel();
-  void updateJointTraj(void);
-
-private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-
-  ros::NodeHandle nh_;
-
-  int fb_left_joy_, lr_left_joy_, square_button_, circ_button_, cross_button_;
-  int dyn_pos_;
-  trajectory_msgs::JointTrajectory new_point_;
-
-  ros::Subscriber joy_sub_;
-  ros::Publisher joint_pub_;
-  // ros::ServiceClient dyn_cmd_client_;
-};
-
-
-Joy2Dynamixel::Joy2Dynamixel():
-  fb_left_joy_(1),
-  lr_left_joy_(0),
-  square_button_(3),
-  circ_button_(1),
-  cross_button_(0),
-  dyn_pos_(ZERO_POS)
-{
-  nh_.param("dynamixel_position", dyn_pos_, dyn_pos_);
-
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Joy2Dynamixel::joyCallback, this);
-  joint_pub_ = nh_.advertise<trajectory_msgs::JointTrajectory>("dynamixel_workbench/joint_trajectory", 1);
-  // dyn_cmd_client_ = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("dynamixel_command");
   
-  new_point_.points.resize(1);
-  new_point_.points[0].positions.resize(1);
-  new_point_.points[0].velocities.resize(1);
-  new_point_.joint_names.push_back("servo");
-  new_point_.points[0].positions[0] = (dyn_pos_-2048)*3.14/2048;
-  new_point_.points[0].velocities[0] = 0.1;
-  new_point_.points[0].time_from_start = ros::Duration(0.2);
+  #define MAX_POS 2550
+  #define ZERO_POS 2048
+  #define MIN_POS 1550
 
-  ROS_INFO("Init");
-}
-
-void Joy2Dynamixel::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
-{
-  int new_pos = dyn_pos_;
-  new_point_.points[0].time_from_start = ros::Duration(0.5);
-
-  if(joy->buttons[square_button_])
-    new_pos = MIN_POS;
-  else if(joy->buttons[cross_button_])
-    new_pos = ZERO_POS;
-  else if(joy->buttons[circ_button_])
-    new_pos = MAX_POS;
-  else
+  class Joy2Dynamixel
   {
-    new_pos = ZERO_POS - (MAX_POS-ZERO_POS)*(joy->axes[lr_left_joy_]);
-    if(new_pos > MAX_POS)
-        new_pos = MAX_POS;
-    if(new_pos < MIN_POS)
-        new_pos = MIN_POS;
+  public:
+    Joy2Dynamixel();
+    void updateJointTraj(void);
+
+  private:
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    void ampCallback(const std_msgs::Int16::ConstPtr& amp);
+
+    ros::NodeHandle nh_;
+
+    int fb_left_joy_, lr_left_joy_, square_button_, circ_button_, cross_button_;
+    int amplitude_, dyn_pos_;
+    trajectory_msgs::JointTrajectory new_point_;
+    
+    auto min_pos = [&a]{ return ZERO_POS - amplitude_/2; };
+    auto max_pos = [&a]{ return ZERO_POS + amplitude_/2; };
+    
+    ros::Subscriber joy_sub_;
+    ros::Subscriber ampli_sub;
+    ros::Publisher joint_pub_;
+    // ros::ServiceClient dyn_cmd_client_;
+  };
+
+
+  Joy2Dynamixel::Joy2Dynamixel():
+    fb_left_joy_(1),
+    lr_left_joy_(0),
+    square_button_(3),
+    circ_button_(1),
+    cross_button_(0),
+    dyn_pos_(ZERO_POS),
+    amplitude_(200)
+  {
+    nh_.param("dynamixel_position", dyn_pos_, dyn_pos_);
+    nh_.param("dynamixel_amplitude", amplitude_, amplitude_);
+
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Joy2Dynamixel::joyCallback, this);
+    ampli_sub_ = nh_.subscribe<std_msgs::Int16>("joy2dynamixel/amplitude", 10, &Joy2Dynamixel::ampCallback, this);
+    joint_pub_ = nh_.advertise<trajectory_msgs::JointTrajectory>("dynamixel_workbench/joint_trajectory", 1);
+    // dyn_cmd_client_ = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("dynamixel_command");
+
+    new_point_.points.resize(1);
+    new_point_.points[0].positions.resize(1);
+    new_point_.points[0].velocities.resize(1);
+    new_point_.joint_names.push_back("servo");
+    new_point_.points[0].positions[0] = (dyn_pos_-2048)*3.14/2048;
+    new_point_.points[0].velocities[0] = 0.1;
+    new_point_.points[0].time_from_start = ros::Duration(0.2);
+
+    ROS_INFO("Init");
   }
 
-  if(new_pos != dyn_pos_)
+  void Joy2Dynamixel::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   {
-    dyn_pos_ = new_pos;
-    ROS_INFO("in joy callback, position: %d", dyn_pos_);
+    int new_pos = dyn_pos_;
 
-    if(ros::ok() && joint_pub_)
+    if(joy->buttons[square_button_])
+      new_pos = min_pos();
+    else if(joy->buttons[cross_button_])
+      new_pos = ZERO_POS;
+    else if(joy->buttons[circ_button_])
+      new_pos = max_pos();
+    else
     {
-      // trajectory_msgs::JointTrajectory new_point;
-      // new_point_.points.resize(1);
-      // new_point_.points[0].positions.resize(1);
-      // new_point_.joint_names.push_back("servo");
-      new_point_.points[0].time_from_start = ros::Duration(0.2);
-      new_point_.points[0].positions[0] = (dyn_pos_-2048)*3.14/2048;
-      // new_point_.points[0].time_from_start = ros::Duration(0.2);
-      // joint_pub_.publish(new_point);
+      new_pos = ZERO_POS - (max_pos()-ZERO_POS)*(joy->axes[lr_left_joy_]);
+      if(new_pos > max_pos())
+          new_pos = max_pos();
+      if(new_pos < min_pos())
+          new_pos = min_pos();
     }
-    // dynamixel_workbench_msgs::DynamixelCommand cmd_srv;
-    // cmd_srv.request.command = "";
-    // cmd_srv.request.id = 1;
-    // cmd_srv.request.addr_name = "Goal_Position";
-    // cmd_srv.request.value = dyn_pos_;
-    // ROS_INFO("%s, %d, %s, %d", cmd_srv.request.command.c_str(), cmd_srv.request.id, cmd_srv.request.addr_name.c_str(), cmd_srv.request.value);
+
+    if(new_pos != dyn_pos_)
+    {
+      dyn_pos_ = new_pos;
+      ROS_INFO("in joy callback, position: %d", dyn_pos_);
+
+      if(ros::ok() && joint_pub_)
+      {
+        new_point_.points[0].positions[0] = (dyn_pos_-2048)*3.14/2048;
+      }
+    }
+  }
   
-    // if(dyn_cmd_client_.call(cmd_srv))
-    //     ROS_INFO("Message sent, position : %d", dyn_pos_);
-    // else
-    //     ROS_INFO_STREAM("Failed to call the dynamixel server" << cmd_srv.response);
-  }
-}
-
-void Joy2Dynamixel::updateJointTraj()
-{
-  joint_pub_.publish(new_point_);
-}
-
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "joy_2_dynamixel");
-  Joy2Dynamixel joy_2_dynamixel;
-
-  ros::Rate loop_rate(20);
-  while (ros::ok())
+  void Joy2Dynamixel::ampCallback(const std_msgs::Int16::ConstPtr& amp)
   {
-    joy_2_dynamixel.updateJointTraj();
-    ros::spinOnce();
-    loop_rate.sleep();
+    amplitude_ = amp->data;
   }
 
-  // ros::spin();
-}
+  void Joy2Dynamixel::updateJointTraj()
+  {
+    joint_pub_.publish(new_point_);
+  }
+
+
+  int main(int argc, char** argv)
+  {
+    ros::init(argc, argv, "joy_2_dynamixel");
+    Joy2Dynamixel joy_2_dynamixel;
+
+    ros::Rate loop_rate(20);
+    while (ros::ok())
+    {
+      joy_2_dynamixel.updateJointTraj();
+      ros::spinOnce();
+      loop_rate.sleep();
+    }
+
+    // ros::spin();
+  }
+} //namespace joy2dynamixel
